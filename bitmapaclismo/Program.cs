fa6dbb282e31697aacaa2b4a2b42148e48150d4e
Philiquaz <Philiquaz@gmail.com>
2024-08-20 23:19:43 +0100

    Reorganise gui, add rgb option and map rescaling.

--- bitmapaclismo/Program.cs
+++ bitmapaclismo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.IO;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Windows.Forms;
 
@@ -9,19 +10,23 @@ namespace bitmapaclismo
     static class bitmapaclismo
     {
         [STAThread]
-        static void Main() {
+        static void Main()
+        {
             
-            try {
+            try
+            {
                 FolderBrowserDialog fbd = new FolderBrowserDialog();
                 fbd.Description = "Open level folder";
                 fbd.UseDescriptionForTitle = true;
                 fbd.ShowDialog();
 
-                if (!Directory.Exists(fbd.SelectedPath)) {
+                if (!Directory.Exists(fbd.SelectedPath))
+                {
                     MessageBox.Show("Could not find folder specified");
                     return;
                 }
-                if (!Path.Exists($@"{fbd.SelectedPath}\terrain")) {
+                if (!Path.Exists($@"{fbd.SelectedPath}\terrain"))
+                {
                     MessageBox.Show("Could not find terrain file in folder specified");
                     return;
                 }
@@ -30,27 +35,22 @@ namespace bitmapaclismo
                 byte[] terrainBytes = File.ReadAllBytes($@"{fbd.SelectedPath}\terrain");
 
                 Terrain terrainFile = new Terrain(new ByteReader(terrainBytes));
-                if (!terrainFile.Validate())
+                if (!terrainFile.Validate() && MessageBox.Show("Terrain file not recognised. Continue?", "", MessageBoxButtons.YesNo) == DialogResult.No)
                 {
-                    MessageBox.Show("Terrain file issue.");
+                    return;
                 }
 
-                int sizex = Util.readInt(terrainBytes, 0x1E -5 + fileName.Length);
-                int sizey = Util.readInt(terrainBytes, 0x22 -5 + fileName.Length);
-                int sizez = Util.readInt(terrainBytes, 0x26 -5 + fileName.Length);
-                int sizeL = Util.readInt(terrainBytes, 0x2A -5 + fileName.Length);
-                int totalSize = Util.readInt(terrainBytes, 0x2E -5 + fileName.Length);
-                int heightwidth = (int)Math.Sqrt(totalSize); //use sqrt for only square map because idk braindamage
-
-                int zRange = 100; //Fixed
-                if (heightwidth != sizex || heightwidth != sizey || zRange != sizez) {
-                    MessageBox.Show("Unexpected size parameters in terrain file - was this already modified?");
+                if (MessageBox.Show("Convert a bitmap image to the terrain file?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    ConvertBitmapToTerrain(fbd.SelectedPath, fileName, terrainFile);
                 }
-
-                if (MessageBox.Show("Choose your action:\n\nYes - Convert terrain file to a bitmap image.\nNo - Convert a bitmap image to the terrain file.", "Select Action", MessageBoxButtons.YesNo) == DialogResult.Yes) {
-                    ConvertTerrainToBitmap(terrainBytes, fbd.SelectedPath, heightwidth);
-                } else {
-                    ConvertBitmapToTerrain(terrainBytes, fileName, fbd.SelectedPath, heightwidth, zRange, fbd);
+                else if (MessageBox.Show("Convert terrain file to a bitmap image?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    ConvertTerrainToBitmap(fbd.SelectedPath, terrainFile);
+                }
+                else
+                {
+                    MessageBox.Show("No option selected. Closing program.", "", MessageBoxButtons.OK);
                 }
             } catch (Exception e)
             {
@@ -58,9 +58,10 @@ namespace bitmapaclismo
             }
         }
 
-        static void ConvertBitmapToTerrain(byte[] terrainBytes, string fileName, string folderPath, int heightwidth, int zRange, FolderBrowserDialog fbd) {
+        static void ConvertBitmapToTerrain(string folderPath, string fileName, Terrain terrain)
+        {
             OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Title = $@"Open {heightwidth}x{heightwidth}px 24bit bitmap";
+            ofd.Title = $@"Open bitmap";
             ofd.ShowDialog();
             if (!Path.Exists(ofd.FileName)) {
                 MessageBox.Show("Could not find bmp file specified");
@@ -68,49 +69,70 @@ namespace bitmapaclismo
             }
             Bitmap bitmap = new Bitmap(ofd.FileName);
 
-            if (MessageBox.Show("Extend height to 255 because you want to break the game?", "", MessageBoxButtons.YesNo) == DialogResult.Yes) {
-                zRange = 255;
+            if (MessageBox.Show("Extend height to 255?\nThis will slow game load times much more than map size.", "", MessageBoxButtons.YesNo) == DialogResult.Yes) {
+                terrain.sizeZ = 255;
             }
 
-            int terrainStart = 0x2d + fileName.Length;
-            int terrainCurrent = terrainStart;
+            if (bitmap.Width != terrain.sizeX || bitmap.Height != terrain.sizeY)
+            {
 
-            for (int x=0; x<heightwidth; x++) {
-                for (int y=0; y<heightwidth; y++) {
+                if (MessageBox.Show("Bitmap is not size of map - map data will be wiped while clearing the map.", "", MessageBoxButtons.OKCancel) == DialogResult.Cancel)
+                    return;
+                if (MessageBox.Show("Bitmap is not square. Border region will be a square based on bitmap's width.\nContinue?", "", MessageBoxButtons.OKCancel) == DialogResult.Cancel)
+                    return;
+                terrain = new Terrain(terrain, fileName, bitmap.Width, bitmap.Height, terrain.sizeZ);
+            }
+
+            for (int x=0; x<terrain.sizeX; x++)
+            {
+                for (int y=0; y<terrain.sizeY; y++)
+                {
                     byte hMapValue = bitmap.GetPixel(x, y).R;
-                    int result = (int)(hMapValue / 255f * zRange);
-                    Util.writeInt(terrainBytes, terrainCurrent, result);
-                    terrainCurrent += 4;
+                    int result = (int)(hMapValue / 255f * terrain.sizeZ);
+                    terrain.height(x, y) = result;
                 }
             }
-            File.WriteAllBytes($@"{fbd.SelectedPath}\terrain", terrainBytes);
-            MessageBox.Show("Finished converting bitmap to terrain.");
+            ByteWriter bw = new ByteWriter();
+            terrain.Write(bw);
+            File.WriteAllBytes($@"{folderPath}\terrain", bw.GetBytes());
         }
         
-        static void ConvertTerrainToBitmap(byte[] terrainBytes, string folderPath, int heightwidth) {
-            Bitmap bitmap = new Bitmap(heightwidth, heightwidth, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+        static void ConvertTerrainToBitmap(string folderPath, Terrain terrain)
+        {
+            
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = $@"Save bitmap";
+            sfd.DefaultExt = "bmp";
+            if (sfd.ShowDialog() != DialogResult.OK)
+                return;
 
-            int terrainStart = 0x2d + Path.GetFileName(folderPath).Length;
-            int terrainCurrent = terrainStart;
+            Bitmap bitmap = new Bitmap(terrain.sizeX, terrain.sizeY, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
 
-            int zRange = 100;
+            bool includeLayers = false;
+            if (MessageBox.Show("Include resource (G), ground type (G) and mist (B) layers?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                includeLayers = true;
 
-            for (int x = 0; x < heightwidth; x++) {
-                for (int y = 0; y < heightwidth; y++) {
-                    int heightValue = Util.readInt(terrainBytes, terrainCurrent);
-                    float normalizedValue = heightValue / (float)zRange;
-                    byte pixelValue = (byte)(normalizedValue * 255);
-
-                    Color color = Color.FromArgb(pixelValue, pixelValue, pixelValue);
+            for (int x = 0; x < terrain.sizeX; x++)
+            {
+                for (int y = 0; y < terrain.sizeY; y++)
+                {
+                    byte heightValue = (byte)(terrain.height(x, y) * 255f / terrain.sizeZ);
+                    Color color;
+                    if (includeLayers)
+                    {
+                        
+                        byte resAndGround = (byte)((byte)terrain.resource(x, y) * 46 - (byte)terrain.groundType(x, y) * 23);
+                        byte mist = (byte)(terrain.mist(x, y) * 255f / terrain.sizeZ);
+                        color = Color.FromArgb(heightValue, resAndGround, mist);
+                    }
+                    else
+                    {
+                        color = Color.FromArgb(heightValue, heightValue, heightValue);
+                    }
                     bitmap.SetPixel(x, y, color);
-
-                    terrainCurrent += 4;
                 }
             }
-
-            string outputPath = Path.Combine(folderPath, "terrain.bmp");
-            bitmap.Save(outputPath, System.Drawing.Imaging.ImageFormat.Bmp);
-            MessageBox.Show($"Terrain converted to bitmap and saved as {outputPath}");
+            bitmap.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
         }
     }
 }
